751134ba12a190feb228e350701d061319f5dd9c
Jan Blaesi <jan@blaesi.com>
2020-09-11 12:56:13 +0200

    polybar: multi display bar, thermal zone adjusted, wifi ip address

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -4,3 +4,8 @@ killall -q polybar
 while pgrep -u $UID -x polxbar >/dev/null; do sleep 1; done
 
 polybar mybar &
+
+if [ "$1" = "qbit-multidisplay" ]; then
+	polybar qbit-multidisplay-left &
+	polybar qbit-multidisplay-right &
+fi
