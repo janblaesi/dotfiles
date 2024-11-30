14774c0cc4849537d4b3d794f9a00815541c443b
Jan Blaesi <jan@blaesi.com>
2024-11-30 15:43:33 +0100

    nvim: add lsp and coq (autocompletion) plugins, set some global settings

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,2 +1,28 @@
 require("config.lazy")
 
+-- Set global options
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+vim.opt.expandtab = true
+vim.opt.number = true
+vim.opt.cursorline = true
+vim.opt.smarttab = true
+vim.opt.guicursor = "a:ver50"
+
+local lualine = require "lualine"
+local neo_tree = require "neo-tree"
+local lsp = require "lspconfig"
+local coq = require "coq"
+
+lualine.setup({
+    options = {
+        globalstatus = true
+    }
+})
+
+neo_tree.setup({})
+
+lsp.clangd.setup(coq.lsp_ensure_capabilities())
+lsp.lua_ls.setup(coq.lsp_ensure_capabilities())
+lsp.gopls.setup(coq.lsp_ensure_capabilities())
+
