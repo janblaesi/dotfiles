d08805e62d93ac7bd5d0b7cf87adef30f5c54e9c
Jan Blaesi <jan.blaesi@qbit.de>
2024-11-30 20:04:17 +0100

    nvim: add tokyonight theme, add barbecue plugin

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,5 +1,7 @@
 require("config.lazy")
 
+vim.cmd[[colorscheme tokyonight-storm]]
+
 -- Set global options
 vim.opt.shiftwidth = 4
 vim.opt.softtabstop = 4
@@ -10,19 +12,28 @@ vim.opt.smarttab = true
 vim.opt.guicursor = "a:ver50"
 
 local lualine = require "lualine"
+local barbecue = require "barbecue"
 local neo_tree = require "neo-tree"
 local lsp = require "lspconfig"
 local coq = require "coq"
 
 lualine.setup({
     options = {
-        globalstatus = true
+        globalstatus = true,
+        theme = "tokyonight-storm"
+    }
+})
+
+barbecue.setup({
+    options = {
+        theme = "tokyonight-storm"
     }
 })
 
 neo_tree.setup({})
 
 lsp.clangd.setup(coq.lsp_ensure_capabilities())
-lsp.lua_ls.setup(coq.lsp_ensure_capabilities())
 lsp.gopls.setup(coq.lsp_ensure_capabilities())
+lsp.lua_ls.setup(coq.lsp_ensure_capabilities())
+lsp.pylsp.setup(coq.lsp_ensure_capabilities())
 
