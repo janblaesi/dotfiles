256e7423b8ec8c263e2e190a8280f2190a961a19
Jan Blaesi <jan.blaesi@qbit.de>
2024-12-06 14:52:18 +0100

    nvim: add some more lsp plugins, add mason to install lsps

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,6 +1,6 @@
 require("config.lazy")
 
-vim.cmd[[colorscheme tokyonight-storm]]
+vim.cmd[[colorscheme tokyonight]]
 
 -- Set global options
 vim.opt.shiftwidth = 4
@@ -11,29 +11,32 @@ vim.opt.cursorline = true
 vim.opt.smarttab = true
 vim.opt.guicursor = "a:ver50"
 
-local lualine = require "lualine"
-local barbecue = require "barbecue"
-local neo_tree = require "neo-tree"
-local lsp = require "lspconfig"
-local coq = require "coq"
-
-lualine.setup({
+require("lualine").setup({
     options = {
         globalstatus = true,
         theme = "tokyonight-storm"
     }
 })
 
-barbecue.setup({
+require("barbecue").setup({
     options = {
         theme = "tokyonight-storm"
     }
 })
 
-neo_tree.setup({})
+require("neo-tree").setup({})
 
+require("mason").setup()
+require("mason-lspconfig").setup()
+
+local lsp = require "lspconfig"
+local coq = require "coq"
 lsp.clangd.setup(coq.lsp_ensure_capabilities())
 lsp.gopls.setup(coq.lsp_ensure_capabilities())
 lsp.lua_ls.setup(coq.lsp_ensure_capabilities())
 lsp.pylsp.setup(coq.lsp_ensure_capabilities())
+lsp.sqlls.setup(coq.lsp_ensure_capabilities())
+lsp.angularls.setup(coq.lsp_ensure_capabilities())
+lsp.ansiblels.setup(coq.lsp_ensure_capabilities())
+lsp.cmake.setup(coq.lsp_ensure_capabilities())
 
